f8ff7cc57970c6d236339092ed4b49f80a896376
luebby <karsten.luebke@fom.de>
2019-09-05 14:59:24 +0200

    binary geht doch nicht...

--- setup/Pakete-installieren.R
+++ setup/Pakete-installieren.R
@@ -1,3 +1,3 @@
 # Installiert die nötigen R Pakete
 
-install.packages(c("mosaic", "lsr", "rmarkdown", "knitr", "rprojroot", "readxl"), type = "binary")
+install.packages(c("mosaic", "lsr", "rmarkdown", "knitr", "rprojroot", "readxl"))
